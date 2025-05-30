c7a982b595a9367b2b3b2aece45a105e0b3b165d
Krish Shaw <shawkrish418@gmail.com>
2025-05-31 04:40:10 +0530

    fix: Graph UI theme responsiveness

--- src/components/PerformanceMetrics.tsx
+++ src/components/PerformanceMetrics.tsx
@@ -2,7 +2,9 @@ import { LineChart } from '@mui/x-charts/LineChart';
 import { useContext } from 'react';
 import { MetricsContext } from '@/context/MetricsStateContext';
 import { TimerContext } from '@/context/TimerStateContext';
+import { useTheme } from './theme-provider';
 export default function GridDemo() {
+  const theme = useTheme();
   const metricsState = useContext(MetricsContext); // wpm and raw 
   const timerState = useContext(TimerContext); // The time to be on x-axis
   if (!metricsState || !timerState) {
@@ -14,20 +16,22 @@ export default function GridDemo() {
   const xAxisData = Array.from({ length }, (_, i) => i + 1);
 
   return (
-    <LineChart
+    <div className={`p-4 rounded-lg ${theme.theme === 'dark' ? 'bg-[#1e1e1e]' : 'bg-white'} transition-colors`}>
+      <LineChart
       xAxis={[{ data: xAxisData, label: 'Time (s)' }]}
+      yAxis={[{ label: 'Words Per Minute' }]}
       series={[
         {
           label: 'WPM',
           curve: 'catmullRom',
           data: wpmArray,
-          color: '#4CAF50', // optional: green
+          color: theme.theme === 'light' ? '#F44336' : '#4CAF50', // optional: green
         },
         {
           label: 'Raw WPM',
           curve: 'catmullRom',
           data: rawArray,
-          color: '#F44336', // optional: red
+          color: theme.theme === 'light' ? '#4CAF50' : '#F44336', // optional: red
         },
       ]}
       height={250}
@@ -35,6 +39,12 @@ export default function GridDemo() {
       grid={{ vertical: true, horizontal: true }}
       className='dark:invert-100'
       axisHighlight = {{ x: 'line' }}
+      sx={{
+        '.MuiChartsLegend-root': {
+          color: theme.theme === 'dark' ? '#fff' : '#000', // Legend label color
+          },
+      }}
     />
+    </div>
   );
 }
